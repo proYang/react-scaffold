e9a56e40646e42b19db8a09d1d59ab1a128309a4
SlaneYang <904509221@qq.com>
2017-11-27 21:09:14 +0800

    :v:更新babel、axios等依赖版本

--- build/build.js
+++ build/build.js
@@ -21,6 +21,12 @@ rm(config.build.distPath, err => {
       chunks: false,
       chunkModules: false
     }) + '\n\n')
+
+    if (stats.hasErrors()) {
+      console.log(chalk.red('  Build failed with errors.\n'))
+      process.exit(1)
+    }
+    
     console.log(chalk.cyan('  Build complete.\n'))
   })
 })
