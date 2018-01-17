11d4d47cc2eaba758ceb4d2edec4cd5499215234
SlaneYang <904509221@qq.com>
2018-01-17 09:35:28 +0800

    test snabbt.js demon

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import snabbt from 'snabbt.js'
+
+import style from './style.less'
+import publishImg from '@/assets/image/publish.png'
+
+const mapStateToProps = () => ({
+})
+
+class Footer extends Component {
+  constructor() {
+    super()
+    this.icon = null
+    this.iconActive = false
+  }
+  componentDidMount() {
+
+  }
+  handleToggleSidebar() {
+    // let { dispatch } = this.props
+  }
+  render() {
+    return (
+      <div className={style.footer}>
+        <img ref={(img) => { this.icon = img }} className={style.icon} src={publishImg} onClick={this.handleClick.bind(this)} />
+        <h2>首页</h2>
+      </div>
+    )
+  }
+  handleClick() {
+    if(this.iconActive === true) {
+      snabbt(this.icon, {
+        fromRotation: [0, 0, -5 / 4 * Math.PI],
+        rotation: [0, 0, 0],
+        easing: 'ease',
+        duration: 500
+      })
+      this.iconActive = false
+    } else {
+      snabbt(this.icon, {
+        rotation: [0, 0, -5 / 4 * Math.PI],
+        easing: 'ease',
+        duration: 500
+      })
+      this.iconActive = true
+    }
+    
+  }
+}
+
+Footer.propTypes = {
+  dispatch: PropTypes.func.isRequired
+}
+
+export default connect(mapStateToProps)(Footer)
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -5,6 +5,7 @@ import style from './style.less'
 import HomeHeader from '@/components/HomeHeader/index'
 import Content from '@/components/Content/index'
 import Sidebar from '@/components/Sidebar/index'
+import Footer from '@/components/Footer/index'
 
 class Home extends Component {
   render() {
@@ -13,6 +14,7 @@ class Home extends Component {
         <HomeHeader />
         <Sidebar />
         <Content id={this.props.match.params.id}/>
+        <Footer />
       </div>
     )
   }
